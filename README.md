29431de96549bce006699dfa7c97dca96dc509a7
Captain Fantasy <captain@fantasyui.com>
2017-08-12 15:07:21 -0400

    Nine color set.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # color-schemes
 Color Scheme Explorer (GUI Application)
+
+![](screenshot-1.png)
+![](screenshot-2.png)
+
+## Installation/Launch
+
+    npm i -g epx;
+    epx color-schemes;
+
+Alternatively, use the app-store.
+
+    npm i -g epx;
+    epx app-store
--- renderer.js
+++ renderer.js
@@ -2,6 +2,8 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+const {clipboard} = require('electron')
+
 const pkg = require(__dirname + '/package.json')
 const EventEmitter = require('events');
 const fs = require('fs');
@@ -45,6 +47,35 @@ var appTitle = new Vue({
   data: pkg
 });
 
+var colors = new Vue({
+  el: '#colors',
+  data: {
+    colors: [
+      {primary: '#E8B691', secondary:'#462B15', text:'#F9EBEC'},
+      {primary: '#B7B7CF', secondary:'#E2402A', text:'#CFDDE8'},
+      {primary: '#FCB180', secondary:'#FF4216', text:'#6D1E00'},
+      {primary: '#99C4BB', secondary:'#666A59', text:'#F4EAB2'},
+      {primary: '#CA972C', secondary:'#B16B1B', text:'#552605'},
+      {primary: '#BAD2BC', secondary:'#CE4E98', text:'#5F0626'},
+      {primary: '#838A6F', secondary:'#854521', text:'#CFBFA1'},
+      {primary: '#98825C', secondary:'#541512', text:'#D5BBBB'},
+      {primary: '#D84B42', secondary:'#192636', text:'#E7D5D1'},
+    ]
+  },
+
+  methods: {
+    flip: function (color) {
+      const tmp = color.primary;
+      color.primary = color.secondary;
+      color.secondary = tmp;
+    },
+    copy: function (color) {
+      clipboard.writeText([color.primary,color.secondary,color.text].join(" "))
+
+    }
+  }
+})
+
 
 
 
@@ -76,7 +107,7 @@ ee.on('document-drop', (data) => {
 
 $(function(){
 
-
+  $('title').text(pkg.productName)
   $( "#emitter-example-form" ).submit(function( event ) {
     const eventName = $( "#event-name-select" ).val();
     const color = $( "#example-color-input" ).val();
